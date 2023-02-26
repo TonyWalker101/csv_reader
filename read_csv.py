a1e82cd589bbe4ab2fd96d7d802d7094041a7081
Tony <awalker780@gmail.com>
2023-02-26 12:35:50 -0500

    implements multiple search parameters feature

--- read_csv.py
+++ read_csv.py
@@ -3,24 +3,28 @@
 import csv
 import sys
 
-#receives search parameters either through arguments on file call or input
+#search = parameters given either through arguments on file call or input
 if len(sys.argv) >= 2:
   search = sys.argv[1:]
 else:
   search = input("Which process are you looking for?\n").split(" ")
 
 def csv_search(search):
-  with open("csv_test.csv") as raw_csv_data:
-    csv_data = csv.DictReader(raw_csv_data)
 
-    for item in search:
-      results = False
+  #for each item in search; open csv, find results, & print results
+  for item in search:
+    results = False
+    
+    with open("csv_test.csv") as raw_csv_data:
+      csv_data = csv.DictReader(raw_csv_data)
+      
       for row in csv_data:
         if item.upper() in row["Process"]:
           print({row["Process"]: {"Controller": row["Controller"], "Backup": row["Backup"], "ENV": row["ENV"]}})
           results = True
 
-      if not results:
-        print(f"No results found for {item}")
+    if not results:
+      print(f"No results found for {item}")
 
+#runs main function
 csv_search(search)
