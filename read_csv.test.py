a21d15d616d6cdcaede5153005b38044eb6b30ab
Tony <awalker780@gmail.com>
2023-03-03 19:09:41 -0500

    creates more test cases

--- read_csv.test.py
+++ read_csv.test.py
@@ -6,9 +6,16 @@ class readCSVTest(unittest.TestCase):
         self.assertEqual(csv_search(""), "No results found for ", "Expected csv_search('') to return 'No results found for'")
 
     def test_process_not_found(self):
+        self.assertEqual(csv_search("NGO777"), "No results found for NGO777", "Expected csv_search('ngo777') to return 'No results found for NGO777")
+
+    def test_process_found(self):
+        self.assertEqual(csv_search("NGO101"), "{'NGO101': {'Controller': 'Tony', 'Backup': 'Bob', 'ENV': '6.5.1'}}", "Expected csv_search('NGO101') to return {'NGO101': {'Controller': 'Tony', 'Backup': 'Bob', 'ENV': '6.5.1'}}")
+
+    def test_capitalizes_input(self):
         self.assertEqual(csv_search("ngo777"), "No results found for NGO777", "Expected csv_search('ngo777') to return 'No results found for NGO777")
 
-# search = "NGO101"
+    #function should find 101 among NGO101, ABC101, etc.
+    def test_search_missing_input(self):
+        self.assertEqual(csv_search("101"), "{'NGO101': {'Controller': 'Tony', 'Backup': 'Bob', 'ENV': '6.5.1'}}", "Expected csv_search('NGO101') to return {'NGO101': {'Controller': 'Tony', 'Backup': 'Bob', 'ENV': '6.5.1'}}")
 
-# csv_search(search)
 unittest.main()
